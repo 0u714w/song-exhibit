e01af6bf4a9ab9ff91378a541e4b3a5e39588169
Doug Enas <douglassenas@gmail.com>
2025-06-26 09:19:13 -0400

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,17 @@
         body { font-family: sans-serif; margin: 2rem; }
         audio { display: block; margin-top: 1rem; }
     </style>
+    <meta property="og:title" content="Song Exhibit" />
+<meta property="og:description" content="3 songs of the same kind" />
+<meta property="og:image" content="{{ asset('images/blue_triangle.png') }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="blue triangle" />
+<meta name="twitter:description" content="songs." />
+<meta name="twitter:image" content="{{ asset('images/blue_triangle.png') }}" />
+
 </head>
 <body>
     <div class="container">
