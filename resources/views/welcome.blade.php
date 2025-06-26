7dc7a50cf8f086e120cba047a2c0ec70455283f1
Doug Enas <douglassenas@gmail.com>
2025-06-26 09:00:49 -0400

    blue triangle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <title>Welcome</title>
 </head>
 <body>
-    <h1>Welcome to the Song Exhibit</h1>
+    <div style="display: flex; align-items: flex-start; gap: 20px;">
     <ul>
+        <h3>GOT YR ARM</h3>
         <li><a href="/dorm-room">Dorm Room Version</a></li>
         <li><a href="/studio">Studio Version</a></li>
         <li><a href="/remix">Remix Version</a></li>
     </ul>
+    <img src="{{ asset('images/blue_triangle.png') }}" alt="blue" style="width: 200px; border-radius: 8px;">
+    </div>
 </body>
 </html>
