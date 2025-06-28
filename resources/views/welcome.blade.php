53cb82d216c8422bf13de46ad1221bb5e640fd04
Doug Enas <douglassenas@gmail.com>
2025-06-28 15:41:17 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,11 @@
 <body>
     <div style="gap: 20px;">
         <img src="{{ asset('images/bucky.png') }}" alt="blue" style="width: 300px; border-radius: 8px;">
+        <img src="{{ asset('images/cat.png') }}" alt="blue" style="width: 200px; border-radius: 8px;">
     <div style="display: flex; align-items: flex-start; gap: 20px; position: relative;">
 
     
-            <p style="font-size: 50px;"> now serving: got yr arm -> </p>
+            <p style="font-size: 45px;"> special songs -> </p>
 
 
     <ul style="list-style: none; padding: 0; margin: 40px 0 0 0;">
@@ -27,10 +28,10 @@
 
 </div>
 
-    <img src="{{ asset('images/red.png') }}" alt="blue" style="width: 200px; border-radius: 8px; margin-right: 120px;">
-    <img src="{{ asset('images/blue_triangle.png') }}" alt="blue" style="width: 200px; border-radius: 8px; margin-right: 120px;">
-    <img src="{{ asset('images/moon.jpeg') }}" alt="blue" style="width: 200px; border-radius: 8px;">
+    
     </div>
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/22xNApk24NTfgwqh1hxOUL?utm_source=generator" width="70%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-</body>
-</html>
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/22SJYaC4CRBMenZXHs3tlN?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></html>
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6sAtdxp52TKhBEJNunvxDF?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4fFisnpJ3AlF9k4kCy8T7t?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/18KhRS8qATd93UVHaYUIHj?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
