c42e632146b65982bb8c8854af6a4e7a4d3b6ac6
Doug Enas <douglassenas@gmail.com>
2025-07-10 15:06:45 -0400

    maybe

--- resources/views/songs/studio.blade.php
+++ resources/views/songs/studio.blade.php
@@ -5,7 +5,7 @@
         <img src="{{ asset('images/jaguar.jpeg') }}" alt="studio" style="width: 200px; border-radius: 8px;">
     <p style="font-size: 35px;">sounds out of nowhere...</p>
     <audio controls>
-        <source src="{{ asset('storage/audio/ghost_wave.mp3') }}" type="audio/mpeg">
+        <source src="{{ asset('storage/audio/studio.mp3') }}" type="audio/mpeg">
         Your browser does not support the audio element.
     </audio>
     
