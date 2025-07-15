e5ca7803dd700d0d45ee5903d8dfbbc6b232818e
Doug Enas <douglassenas@gmail.com>
2025-07-15 03:18:29 -0400

    clue

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,16 +7,16 @@
         body { font-family: sans-serif; margin: 2rem; }
         audio { display: block; margin-top: 1rem; }
     </style>
-    <meta property="og:title" content="Song Exhibit" />
-<meta property="og:description" content="3 songs of the same kind" />
-<meta property="og:image" content="{{ asset('images/blue_triangle.png') }}" />
+    <meta property="og:title" content="bucky gets a clue" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="{{ asset('images/pokemon2.png') }}" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ url()->current() }}" />
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:title" content="blue triangle" />
 <meta name="twitter:description" content="songs." />
-<meta name="twitter:image" content="{{ asset('images/blue_triangle.png') }}" />
+<meta name="twitter:image" content="{{ asset('images/pokemon2.png') }}" />
 
 </head>
 <body>
